d6e51f23e08adbcfc04b77e839a2faa3a9b40d21
jeroen <vo.jeroen@gmail.com>
2016-08-01 21:46:49 +0200

    add timestamp columns

--- nimbus/models/__init__.py
+++ nimbus/models/__init__.py
@@ -1,6 +1,9 @@
 import uuid
 
+import datetime
+import pytz
 from sqlalchemy import Column, ForeignKey
+from sqlalchemy import DateTime
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -9,8 +12,14 @@ from nimbus.models.types import UUID
 from nimbus.settings import SQL_ENGINE_URL
 
 
+def get_utc_timestamp():
+    return pytz.utc.localize(datetime.datetime.utcnow())
+
+
 class Base(object):
     uuid = Column(UUID, primary_key=True, default=uuid.uuid4)
+    timestamp_created = Column(DateTime, default=get_utc_timestamp)
+    timestamp_updated = Column(DateTime, default=get_utc_timestamp, onupdate=get_utc_timestamp)
 
     @property
     def uuid_str(self):
