a2e958bf5ddd28f66a3822a4381f46742515ecff
jeroen <vo.jeroen@gmail.com>
2017-02-08 22:11:01 +0100

    allow for historization tables

--- nimbus/models/__init__.py
+++ nimbus/models/__init__.py
@@ -5,6 +5,7 @@ import uuid
 import pytz
 from sqlalchemy import Column
 from sqlalchemy import DateTime
+from sqlalchemy import String
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -34,6 +35,18 @@ class Base(object):
     @property
     def uuid_str(self):
         return str(self.uuid)
+
+
+class History:
+    _history_uuid = Column(UUID, primary_key=True, default=uuid.uuid4)
+    _history_action = Column(String, nullable=False)
+    uuid = Column(UUID, nullable=False)
+    timestamp_updated = Column(DateTime, nullable=False)
+
+    def __init__(self, old_obj):
+        columns = old_obj.__class__.__table__.columns
+        for column in columns:
+            setattr(self, column.name, getattr(old_obj, column.name))
 
 
 Base = declarative_base(cls=Base)
